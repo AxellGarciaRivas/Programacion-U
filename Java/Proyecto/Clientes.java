5317aaa82cd7c879bada2e719ffbf60c3f2a0400
Axell Garcia Rivas <72206592+AxellGarciaRivas@users.noreply.github.com>
2023-03-06 18:38:34 -0600

    agregando archivos para proyectos

---
+++ Java/Proyecto/Clientes.java
@@ -0,0 +1,5 @@
+package Java.Proyecto;
+
+public class Clientes {
+    
+}
---
+++ Java/Proyecto/Proyecto.java
@@ -0,0 +1,7 @@
+package Java.Proyecto;
+
+public class Proyecto {
+    public static void main(String[] args) {
+        
+    }
+}
